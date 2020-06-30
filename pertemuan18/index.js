05432c10e05ebf3ec9d736b040581acd26bad4d0
lorwens21 <lorwensjr21@gmail.com>
2020-06-30 14:10:38 +0700

    menambahkan pertemuan 18

---
+++ pertemuan18/index.js
@@ -0,0 +1,81 @@
+// 1. Menyisipkan JavaScript Di HTML
+
+//console.log("Hello world dari External JS");
+
+//let firstName="John";
+//let $number= 3;
+//let years;
+//var age="23";
+//let isMarried= false;
+
+//firstName="Jane";
+
+
+//console.log(firstName);
+//console.log($number);
+//console.log(isMarried);
+//console.log(years);
+
+// 2. Operator Aritmatika
+
+//Operator Aritmatika
+// let num1=10;
+// let num2=2;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 ** num2);
+// num++;
+// console.log(num1);
+// num1--;
+// console.log(num1);
+
+//Assignment Operator
+
+// let num3=10;
+// let num4=2;
+
+// num4=num3;
+// console.log(num3);
+
+// num3 **= 2;
+
+//String Operator
+
+// let firstName="John";
+// let lastName="Doe";
+// let age=25;
+
+// console.log(firstName + " " + lastName);
+// console.log(firstName + age);
+
+//Operator Perbandingan
+// let num1 =10;
+
+// console.log(num1 == 10);
+// console.log(num1 == "10");
+
+// console.log(num1 === 10);
+// console.log(num1 === "10");
+
+// console.log(num1 >= 10);
+// console.log(num1 <= 10);
+
+// let num2 = num1 === 10 ? "Ten" :  "Another number";
+// console.log(num2);
+
+//Operator Logika
+let hashCertificate= false;
+let hashTranscript= false;
+
+console.log(hashCertificate || hashTranscript);
+console.log(!hashTranscript);
+
+// Operator Precedence
+
+let result= (10 + 2) * 3;
+
+console.log(result);
---
+++ pertemuan18/script.js
@@ -0,0 +1,15 @@
+let mark_kg=90;
+let mark_meter=150;
+
+let john_kg=70;
+let john_meter=170;
+
+let bmi1 = mark_kg / (mark_meter * mark_meter);
+let bmi2 = john_kg / (john meter * john_meter) ;
+
+console.log(bmi1);
+console.log(bmi2);
+
+let isMark_BMI_higherThanJohn = true;
+
+console.log(isMark_BMI_higherThanJohn);
